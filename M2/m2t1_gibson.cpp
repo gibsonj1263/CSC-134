406d4545a33522481267c32c256806f16b88a398
gibsonj1263 <gibsonj1263@student.faytechcc.edu>
2026-09-14 14:46:30 +0000

    m2t2 start

---
+++ M2/m2t1_gibson.cpp
@@ -0,0 +1,32 @@
+// CSC 134
+// M2T1 - Receipt
+// Gibson
+// 9/14/26
+// build a receipt that looks like a receipt
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // Declare Variables
+    string meal_name;  // ex. chicken sandwich 
+    double meal_price; // $
+    double tax_rate;   // percent
+    double tax_amount; // in USD
+    double total;      // $, meal + tax
+    // Input
+    meal_name = "chicken sandwich";
+    meal_price = 5.99;
+    tax_rate = 0.08;    // 8%
+    
+
+
+    // Processing
+
+    // Output
+
+
+    return 0;
+}
