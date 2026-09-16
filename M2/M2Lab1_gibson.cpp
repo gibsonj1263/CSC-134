4b82f5d6dc6a59bcad53c5eefcf1b59c5d2e915e
gibsonj1263 <gibsonj1263@student.faytechcc.edu>
2026-09-16 14:50:08 +0000

    M2Lab1 finished

--- M2/M2Lab1_gibson.cpp
+++ M2/M2Lab1_gibson.cpp
@@ -10,14 +10,44 @@ using namespace std;
 int main() {
 
 // Declare Constants and Variables
+const double Cost_Per_Cubic_Foot = 0.23;
+const double Charge_Per_Cubic_Foot = 0.50;
+
+// Variables describing the crate
+double length, width, height;       // you can declare multiple of same type at once
+double volume;
+double crate_cost;
+double crate_charge;
+double profit;
 
 // Get dimensions of the crate
+cout << "Please enter the crate dimensions." << endl;
+cout << "Crate Length: ";
+cin >> length;
+cout << "Crate Width: ";
+cin >> width;
+cout << "Crate Height: ";
+cin >> height;
+
+// Calculate the volume of the crate (important)
+volume = length * width * height;
 
 // Calculate price and cost
+crate_cost = Cost_Per_Cubic_Foot * volume;
+crate_charge = Charge_Per_Cubic_Foot * volume;
 
 // Calculate profit (price - cost)
+profit = crate_charge - crate_cost;
 
 // Display results to the user
+cout << setprecision(2) << fixed;
+cout << "A crate measuring " << length << " x " << width << " x " << height << " ft. " << endl;
+cout << "Is volume: " << volume << " cubic ft." << endl;
+cout << endl;
+cout << "Cost to build: $" << crate_cost << endl;
+cout << "Sells for:     $" << crate_charge << endl;
+cout << "Profit:        $" << profit << endl;
+
 
 
 return 0; // no errors
