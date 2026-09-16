38388e65198fb2477d99351bd8a2faf21dd6570e
gibsonj1263 <gibsonj1263@student.faytechcc.edu>
2026-09-16 14:57:26 +0000

    M2Lab1 actually finished

--- M2/M2Lab1_gibson.cpp
+++ M2/M2Lab1_gibson.cpp
@@ -10,15 +10,15 @@ using namespace std;
 int main() {
 
 // Declare Constants and Variables
-const double Cost_Per_Cubic_Foot = 0.23;
-const double Charge_Per_Cubic_Foot = 0.50;
+const double Cost_Per_Cubic_Foot = 0.23;        // (Material and fabrication cost per cu ft, our cost to make)
+const double Charge_Per_Cubic_Foot = 0.50;      // (Billed invoice amount, customer cost)
 
 // Variables describing the crate
 double length, width, height;       // you can declare multiple of same type at once
-double volume;
-double crate_cost;
-double crate_charge;
-double profit;
+double volume;                      // V = l * w * h, in cubic ft
+double crate_cost;                  // price to make the crate, USD per cubic ft
+double crate_charge;                // price we sell it for, USD per cubic ft
+double profit;                      // charge - cost
 
 // Get dimensions of the crate
 cout << "Please enter the crate dimensions." << endl;
@@ -29,8 +29,8 @@ cin >> width;
 cout << "Crate Height: ";
 cin >> height;
 
-// Calculate the volume of the crate (important)
-volume = length * width * height;
+// Calculate the volume of the crate (very important for everything else)
+volume = length * width * height;       // cubic feet
 
 // Calculate price and cost
 crate_cost = Cost_Per_Cubic_Foot * volume;
@@ -40,7 +40,7 @@ crate_charge = Charge_Per_Cubic_Foot * volume;
 profit = crate_charge - crate_cost;
 
 // Display results to the user
-cout << setprecision(2) << fixed;
+cout << setprecision(2) << fixed;       // 2 decimals for all values
 cout << "A crate measuring " << length << " x " << width << " x " << height << " ft. " << endl;
 cout << "Is volume: " << volume << " cubic ft." << endl;
 cout << endl;
