14b321588e0ccbb56e15baa1d095caf8cc0c32d9
gibsonj1263 <gibsonj1263@student.faytechcc.edu>
2026-09-16 14:28:33 +0000

    M2Lab1 start

---
+++ M2/M2Lab1_gibson.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+// M2Lab1 - Crates
+// Gibson
+// 9/16/26
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+// Declare Constants and Variables
+
+// Get dimensions of the crate
+
+// Calculate price and cost
+
+// Calculate profit (price - cost)
+
+// Display results to the user
+
+
+return 0; // no errors
+}
