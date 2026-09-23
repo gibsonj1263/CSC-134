9ceed2819f8c346b8f4d1cfd77708a6839fa3714
gibsonj1263 <gibsonj1263@student.faytechcc.edu>
2026-09-23 15:13:36 +0000

    m3t2 done

---
+++ M2/m3t2_gibson.cpp
@@ -0,0 +1,46 @@
+/*
+CSC - 134
+M3T2 - Random Numbers
+Gibson
+9/23/26
+Start writing a simple "Craps" casino game
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+    // random number generator
+    // rand() needs to be seeded with srand()
+    // most "random" is to use the time
+    srand(time(0));
+    // cout << rand() << " " << rand() << endl;
+    // to get a number in a usable size, use % (modulo)
+    int roll1 = (rand() % 6) + 1; // mod 6 is 0-5, so add one for 1-6
+    int roll2 = (rand() % 6) + 1;
+    int total = roll1 + roll2;
+    int point;
+    cout << "You rolled: " << roll1 << " + " << roll2 << " = " << total << endl;
+
+    // Processing -- figure out win, lose, or point
+    // || is OR -- both sides must be full Boolean expressions (&& is AND)
+    if (total == 7 || total == 11) {
+        cout << "YOU WIN!" << endl;
+    }
+    else if (total == 2 || total == 3 || total == 12) {
+        cout << "YOU LOSE!" << endl;
+    }
+    else {
+        //point number (TODO in module 4)
+        point = total; // last roll becomes the point number
+        cout << "Your point number is: " << point << endl;
+        cout << "Keep Rolling: point is win, 7 is loss." << endl;
+        // we'll finish this later because it requires a loop.
+    }
+
+
+    return 0;
+}
