58ad4b24b89e2844d940904bf60547c03891cc89
gibsonj1263 <gibsonj1263@student.faytechcc.edu>
2026-09-23 14:28:16 +0000

    m2hw1 continuation

--- M2/m2hw1_gibson.cpp
+++ M2/m2hw1_gibson.cpp
@@ -20,9 +20,9 @@ void question4();
 
 int main() {
     // Run only the questions you finish by removing the // 
-    question1();
+    //question1();
     //question2();
-    //question3();
+    question3();
     //question4();
 }
 
@@ -61,11 +61,61 @@ void question1() {
 }
 
 void question2() {
-    cout << "Question 2 goes here" << endl;
+    cout << "Solid Snake Crates Inc. (Economic Shift)" << endl;
+
+    // Declare constants
+    const double Cost_Per_Cubic_Foot = 0.30;        // (Material and fabrication cost per cu ft, our cost to make) (Changed from 0.23)
+    const double Charge_Per_Cubic_Foot = 0.52;      // (Billed invoice amount, customer cost) (Changed from 0.50)
+
+    // Declare Variables
+    double length, width, height;       // you can declare multiple of same type at once
+    double volume;                      // V = l * w * h, in cubic ft
+    double crate_cost;                  // price to make the crate, USD per cubic ft
+    double crate_charge;                // price we sell it for, USD per cubic ft
+    double profit;                      // charge - cost
+
+    // Get dimensions of the crate
+    cout << "Please enter the crate dimensions." << endl;
+    cout << "Crate Length: ";
+    cin >> length;
+    cout << "Crate Width: ";
+    cin >> width;
+    cout << "Crate Height: ";
+    cin >> height;
+
+    // Calculate the volume of the crate (very important for everything else)
+    volume = length * width * height;       // cubic feet
+
+    // Calculate price and cost
+    crate_cost = Cost_Per_Cubic_Foot * volume;
+    crate_charge = Charge_Per_Cubic_Foot * volume;
+
+    // Calculate profit (price - cost)
+    profit = crate_charge - crate_cost;
+
+    // Display results to the user
+    cout << setprecision(2) << fixed;       // 2 decimals for all values
+    cout << "A crate measuring " << length << " x " << width << " x " << height << " ft. " << endl;
+    cout << "Is volume: " << volume << " cubic ft." << endl;
+    cout << endl;
+    cout << "Cost to build: $" << crate_cost << endl;
+    cout << "Sells for:     $" << crate_charge << endl;
+    cout << "Profit:        $" << profit << endl;
+
 }
 
 void question3() {
-    cout << "Question 3 goes here" << endl;
+    cout << "Pizza Party Slice Calculator" << endl;
+
+    // Declare Variables
+    int pizzas_ordered;
+
+    // Input
+
+    // Processing
+
+    // Output
+
 }
 
 void question4() {
